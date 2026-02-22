50589287709e3b72523c631c19f6c489bb15820f
Ibsa-Magarsa <your-real-email@gmail.com>
2026-02-22 13:13:57 +0300

    date added and edited on features

--- To_Do.js
+++ To_Do.js
@@ -1,6 +1,7 @@
 const todoInput = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
+const todoDate = document.getElementById("todoDate");
 const totalCount = document.getElementById("totalCount");
 const completedCount = document.getElementById("completedCount");
 const clearBtn = document.getElementById("clearBtn");
@@ -11,16 +12,22 @@ let completedTodos = 0;
 // Add Todo
 addBtn.addEventListener("click", () => {
   const text = todoInput.value.trim();
+  const dateValue = todoDate.value;
 
   if (text === "") {
     alert("Please enter a todo!");
     return;
   }
-
+  
   const li = document.createElement("li");
 
   const span = document.createElement("span");
   span.textContent = text;
+
+  const dateSpan = document.createElement("span");
+  dateSpan.textContent = dateValue ||"Has No date";
+  dateSpan.classList.add("date");
+
 
   const completeBtn = document.createElement("button");
   completeBtn.textContent = "Complete";
@@ -29,6 +36,7 @@ addBtn.addEventListener("click", () => {
   deleteBtn.textContent = "Delete";
 
   li.appendChild(span);
+  li.appendChild(dateSpan);
   li.appendChild(completeBtn);
   li.appendChild(deleteBtn);
   todoList.appendChild(li);
@@ -37,6 +45,7 @@ addBtn.addEventListener("click", () => {
   updateCounts();
 
   todoInput.value = "";
+  todoDate.value = "";
 
   // Complete Todo
   completeBtn.addEventListener("click", () => {
